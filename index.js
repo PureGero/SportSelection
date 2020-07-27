a194adcfe6eeaa6887f0b51b18947ed0f91a08b5
PureGero <puregero@gmail.com>
2020-07-27 15:46:28 +0000

    Add workers to config.json

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (!fs.existsSync('config.json')) {
 
 const config = require('./config.json');
 
-const clusterWorkerSize = os.cpus().length;
+let clusterWorkerSize = config.workers || os.cpus().length;
 
 if (cluster.isMaster) {
     let workers = [];
